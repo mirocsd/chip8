1f658a75d699eaefa822382ae05a7e7db33b2c68
mirocsd <mirostrasz@gmail.com>
2024-07-10 21:42:47 -0600

    Started input handling

--- chip8.cc
+++ chip8.cc
@@ -14,9 +14,10 @@ typedef struct {
   SDL_Texture *tex;
   SDL_Renderer *rend;
 } sdl_t;
-void handle_signal(int signal) {
+
+void handle_extern_signal(int signal) {
   if (signal == SIGINT || signal == SIGTERM || signal == SIGTSTP) {
-    std::cout << "Exiting..." << std::endl;
+    std::cout << "\nExiting..." << std::endl;
     SDL_Quit();
     exit(0);
   }
@@ -27,37 +28,27 @@ void init_sdl(sdl_t *sdl) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
       printf("error initializing SDL: %s\n", SDL_GetError());
   }
-  
-
   // Defining window width and height
-  int SCREEN_WIDTH = 640;
-  int SCREEN_HEIGHT = 320;
+  int SCREEN_WIDTH = 1280;
+  int SCREEN_HEIGHT = 640;
   sdl->win = SDL_CreateWindow("CHIP8", 
                                      SDL_WINDOWPOS_CENTERED, 
                                      SDL_WINDOWPOS_CENTERED, 
                                      SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-
   // Defining window resolution
   int RES_WIDTH = 64;
   int RES_HEIGHT = 32;
-
-
   // Creating renderer
   sdl->rend = SDL_CreateRenderer(sdl->win, -1, SDL_RENDERER_ACCELERATED);
-
-
   // Defining foreground/background colors
-
   const uint8_t FG_R = 0xFF;
   const uint8_t FG_G = 0xFF;
   const uint8_t FG_B = 0xFF;
   const uint8_t FG_A = 0xFF;
   const uint8_t BG_R = 0x00;
-  const uint8_t BG_G = 0x00;
+  const uint8_t BG_G = 0xFF;
   const uint8_t BG_B = 0x00;
   const uint8_t BG_A = 0x00;
-
-
   SDL_SetRenderDrawColor(sdl->rend, BG_R, BG_G, BG_B, BG_A);
   // Clear the screen
   SDL_RenderClear(sdl->rend);
@@ -85,9 +76,9 @@ void update_screen(sdl_t *sdl) {
 }
 // Register the signal handler for stopping with CTRL+Z/C
 void init_sighandle(void) {
-  signal(SIGINT, handle_signal);
-  signal(SIGTERM, handle_signal);
-  signal(SIGTSTP, handle_signal);
+  signal(SIGINT, handle_extern_signal);
+  signal(SIGTERM, handle_extern_signal);
+  signal(SIGTSTP, handle_extern_signal);
 }
 
 void exit_cleanup(sdl_t *sdl) {
@@ -97,16 +88,45 @@ void exit_cleanup(sdl_t *sdl) {
   SDL_DestroyWindow(sdl->win);
 }
 
+void handle_input(int &chip8_state) {
+  SDL_Event event;
+
+  while (SDL_PollEvent(&event)) {
+    switch(event.type) {
+      case SDL_QUIT:
+        chip8_state = 0;
+        return;
+      
+      case SDL_KEYDOWN:
+        switch (event.key.keysym.sym) {
+          case SDLK_ESCAPE:
+            chip8_state = 0;
+            return;
+          default:
+            break;
+        }
+        break;
+
+      case SDL_KEYUP:
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
 int main() {
+  int chip8_state = 1; // 1 for running, 0 for stopped, (2 for paused?)
   init_sighandle();
   
   sdl_t sdl = {0};
   init_sdl(&sdl);
   
   // Main loop
-  while(1) {
-
-    // Delays for 60Hz, should compensate for time spent executing CHIP8 instructions later on
+  while(chip8_state != 0) {
+    handle_input(chip8_state);
+    // Delays for 60Hz, should compensate for time spent executing CHIP8 instructions as well
     SDL_Delay(1000/60);
 
     update_screen(&sdl);
